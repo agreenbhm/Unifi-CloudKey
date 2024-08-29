10602f454b7c915ae3684a09d0bcf36a48e954cf
Drew Green <agreenbhm@gmail.com>
2024-08-29 15:52:30 -0400

    Updated vm provision script to install ubnt-systool

--- provision_vm.sh
+++ provision_vm.sh
@@ -8,12 +8,19 @@ fi
 
 apt update
 apt install -yf nginx php-fpm
-mv /etc/nginx /etc/nginx.old
+mv -f /etc/nginx /etc/nginx.old
 cp -R cloudkey_stock/etc/* /etc/
 cp -R cloudkey_stock/usr/* /usr/
+cp cloudkey_stock/sbin/ubnt-systool /sbin/
+chmod +x /sbin/ubnt-systool
 chmod 755 /etc/ssl/private
 sed -i 's/fastcgi_pass unix:\/var\/run\/php5-fpm.sock;/fastcgi_pass unix:\/var\/run\/php\/php-fpm.sock;/g'  /etc/nginx/sites-enabled/cloudkey-webui
 sed -i 's/\$trline{0}/\$trline\[0\]/g' /usr/share/cloudkey-webui/www/common.inc
 systemctl restart nginx
+
+if [ "$1" == "test" ]
+then
+	exit 0
+fi
 
 ./provision_cloudkey.sh vm
