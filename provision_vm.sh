776145f1a70bb9dc0d200e8714c0de538d390142
Drew Green <agreenbhm@gmail.com>
2024-08-29 14:32:13 -0400

    Update provision_vm.sh

--- provision_vm.sh
+++ provision_vm.sh
@@ -11,6 +11,7 @@ apt install -yf nginx php-fpm
 mv /etc/nginx /etc/nginx.old
 cp -R cloudkey_stock/etc/* /etc/
 cp -R cloudkey_stock/usr/* /usr/
+chmod 755 /etc/ssl/private
 
 sed -i 's/fastcgi_pass unix:\/var\/run\/php5-fpm.sock;/fastcgi_pass unix:\/var\/run\/php\/php-fpm.sock;/g'  /etc/nginx/sites-enabled/cloudkey-webui
 
