fdb4a10a49475bfaf0672a301af13c0368db8392
Drew Green <agreenbhm@gmail.com>
2024-08-29 13:15:22 -0400

    Added provisioning script for vm test environment.

---
+++ provision_vm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0]
+then
+	echo "Please re-run with root privileges."
+	exit
+fi
+
+apt update
+apt install -yf nginx php-fpm
+mv /etc/nginx /etc/nginx.old
+cp -R cloudkey_stock/etc/* /etc/
+cp -R cloudkey_stock/usr/* /usr/
+
+sed -i 's/fastcgi_pass unix:\/var\/run\/php5-fpm.sock;/fastcgi_pass unix:\/var\/run\/php\/php-fpm.sock;/g'  /etc/nginx/sites-enabled/cloudkey-webui
