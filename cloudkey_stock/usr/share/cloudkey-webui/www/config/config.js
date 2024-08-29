368ed602515b714942b3e9f49558acb8acb2b010
Drew Green <agreenbhm@gmail.com>
2024-08-29 12:08:03 -0400

    Added stock cloudkey web files as backup

---
+++ cloudkey_stock/usr/share/cloudkey-webui/www/config/config.js
@@ -0,0 +1,7 @@
+window.config = {
+  ApiEndpoint: '/api',
+  LoginEndpoint: '/login.php',
+  LogoutEndpoint: '/logout.php',
+  InfoEndpoint: '/info.php',
+  CheckFwUpdateEndpoint: '/api/check-update-firmware'
+};
