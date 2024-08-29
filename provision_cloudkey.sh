902b802d4d41285b72f1ae155f97126c896aa93d
Drew Green <agreenbhm@gmail.com>
2024-08-29 11:59:08 -0400

    Updated provisioning script to make output more readable

--- provision_cloudkey.sh
+++ provision_cloudkey.sh
@@ -1,53 +1,81 @@
 #!/bin/bash
 
-echo "Disabling stock Unifi and database..."
+# Info messages in yellow
+info() {
+    command echo -e "\e[1;33m$@\e[0m"
+}
+
+# Success messages in green
+success() {
+    command echo -e "\e[1;32m$@\e[0m\n\n"
+}
+
+# Error messages in red
+error() {
+    echo -e "\e[1;31m$@\e[0m" >&2
+}
+exec 2> >(while read -r line; do error "$line"; done)
+
+# Shutting off pre-installed Unifi and MongoDB
+info "Disabling stock Unifi and database..."
 systemctl disable unifi && systemctl stop unifi
 systemctl disable mongodb && systemctl stop mongodb
-echo "Done"
+success "Done"
 
-echo "Backing up repo lists and installing new ones..."
+# Existing Debian repo lists are outdated and don't work; replacing with working ones and adding additional ones
+info "Backing up repo lists and installing new ones..."
 mv /etc/apt/sources.list /etc/apt/sources.list.old
 mv /etc/apt/sources.list.d /etc/apt/sources.list.d.old
 echo 'deb https://download.docker.com/linux/debian jessie stable' > /etc/apt/sources.list
 echo 'deb [ arch=amd64,arm64 ] https://www.ui.com/downloads/unifi/debian stable ubiquiti' >> /etc/apt/sources.list
 echo 'deb http://archive.debian.org/debian-security jessie/updates main contrib non-free' >> /etc/apt/sources.list
 echo 'deb http://archive.debian.org/debian/ jessie main contrib non-free' >> /etc/apt/sources.list
-echo "Done"
+success "Done"
 
-echo "Updating apt and installing Docker and dependencies..."
+info "Updating apt and installing Docker and dependencies..."
+# fixing missing /dev/tty[0-9] requirement of some dependency. 
+#This doesn't do anything, but fixes install/config errors.
 touch /dev/tty0
+# moving Docker dir to /srv/ partition to account for limited userdata space
+mkdir -p /srv/var/lib/docker
+ln -sf /srv/var/lib/docker /var/lib/docker
 apt update && apt -yf install docker-ce
-echo "Done"
+success "Done"
 
-echo "Downgrading Docker to working version and installing docker-compose..."
+# There's some weird issue introduced with Docker 18.06.2 to address a CVE which 
+# breaks Docker on this device.  18.06.1 works though.  Installing latest from repo and then 
+# downgrading from .deb allows installation of dependencies.
+info "Downgrading Docker to working version and installing docker-compose..."
 wget https://download.docker.com/linux/debian/dists/jessie/pool/stable/armhf/docker-ce_18.06.1~ce~3-0~debian_armhf.deb
 dpkg -i docker-ce_18.06.1~ce~3-0~debian_armhf.deb
 wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/docker-compose-plugin_2.6.0~debian-buster_armhf.deb
 dpkg -i docker-compose-plugin_2.6.0~debian-buster_armhf.deb
 ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/bin/
-echo "Done"
+success "Done"
 
-echo "Making changes to host system webui to support new Unifi version in Docker..."
+
+# Checking Unifi version via the MongoDB container so version is still available if the Unifi 
+# container is stopped, which is necessary for the webui.
+# Changing service start/stop functions to call Docker instead of the host's service.
+# Known Issue - the Maintenance page might show the service as stopped for an extended length
+# of time after clicking 'Start' as it takes a while for the container to start.
+info "Making changes to host system webui to support new Unifi version in Docker..."
 usermod -a -G docker www-data
 cp /usr/share/cloudkey-webui/www/common.inc /usr/share/cloudkey-webui/www/common.inc.bak
 cp /usr/share/cloudkey-webui/www/settings.inc /usr/share/cloudkey-webui/www/settings.inc.bak
 cp /usr/share/cloudkey-webui/www/api.inc /usr/share/cloudkey-webui/www/api.inc.bak
-
-#checking version via the mongodb container so version is still available if the unifi package is stopped
-# Known Issue - the Maintenance page might show the service as stopped for an extended length of time after clicking 'Start' as it takes a while for the container to start
 #sed -i "s/\$cmd = 'dpkg-query/\$cmd = 'docker exec -t unifi dpkg-query/g" /usr/share/cloudkey-webui/www/common.inc
 sed -i "s/\$cmd = 'dpkg-query/\$cmd = 'docker exec -t mongodb dpkg-query/g" /usr/share/cloudkey-webui/www/common.inc
 sed -i 's/^exit 0$/touch \/var\/run\/unifi_runtime.cfg\nexit 0/g' /etc/rc.local
 sed -i "s/.*CMD_SERVICE_UNIFI.*/define('CMD_SERVICE_UNIFI', '\/usr\/bin\/docker ');/g" /usr/share/cloudkey-webui/www/settings.inc
 sed -i "s/exec(CMD_SERVICE_UNIFI . ' start', \$out, \$rc);/exec(CMD_SERVICE_UNIFI . ' start unifi', \$out, \$rc);/g" /usr/share/cloudkey-webui/www/api.inc
 sed -i "s/exec(CMD_SERVICE_UNIFI . ' stop', \$out, \$rc);/exec(CMD_SERVICE_UNIFI . ' stop unifi', \$out, \$rc);/g" /usr/share/cloudkey-webui/www/api.inc
+success "Done"
 
-echo "Done"
-
-echo "Building Docker image - this will probably take a while..."
+info "Building Docker image - this will probably take a while..."
 docker build -t agreenbhm/unifi-cloudkey:8.4.59 --network host .
-echo "Done"
+success "Done"
 
-echo "Starting containers..."
+info "Starting containers..."
 docker-compose up -d
-echo "Finished!"
+success "Finished!"
