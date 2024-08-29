498d38ee48b4e1f776e9204b701d2849a9ca0a9f
Drew Green <agreenbhm@gmail.com>
2024-08-29 15:41:32 -0400

    Update provision_vm.sh

--- provision_vm.sh
+++ provision_vm.sh
@@ -13,6 +13,7 @@ cp -R cloudkey_stock/etc/* /etc/
 cp -R cloudkey_stock/usr/* /usr/
 chmod 755 /etc/ssl/private
 sed -i 's/fastcgi_pass unix:\/var\/run\/php5-fpm.sock;/fastcgi_pass unix:\/var\/run\/php\/php-fpm.sock;/g'  /etc/nginx/sites-enabled/cloudkey-webui
+sed -i 's/\$trline{0}/\$trline\[0\]/g' /usr/share/cloudkey-webui/www/common.inc
 systemctl restart nginx
 
 ./provision_cloudkey.sh vm
