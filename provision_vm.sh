f05a15095b10b801867d6d1c9ea0370503431632
Drew Green <agreenbhm@gmail.com>
2024-08-29 16:54:54 -0400

    Update provision_vm.sh

--- provision_vm.sh
+++ provision_vm.sh
@@ -19,6 +19,8 @@ cp -R cloudkey_stock/etc/* /etc/
 cp -R cloudkey_stock/usr/* /usr/
 cp cloudkey_stock/sbin/* /sbin/
 chmod +x /sbin/ubnt-systool /sbin/chpasswd /sbin/pwcheck /sbin/ubnt-tools /sbin/ubnt-systemhub
+systemctl enable ubnt-systemhub
+systemctl start ubnt-systemhub
 chmod 755 /etc/ssl/private
 sed -i 's/fastcgi_pass unix:\/var\/run\/php5-fpm.sock;/fastcgi_pass unix:\/var\/run\/php\/php-fpm.sock;/g'  /etc/nginx/sites-enabled/cloudkey-webui
 sed -i 's/\$trline{0}/\$trline\[0\]/g' /usr/share/cloudkey-webui/www/common.inc
